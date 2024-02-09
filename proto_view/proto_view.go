659226c581866a665efbd8667f1e3ce17e33413a
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2024-02-09 14:36:21 +0900

    add Demand monitor

--- proto_view/proto_view.go
+++ proto_view/proto_view.go
@@ -3,6 +3,7 @@ package proto_view
 import (
 	"fmt"
 	"log"
+
 	api "github.com/synerex/synerex_api"
 	sxutil "github.com/synerex/synerex_sxutil"
 )
@@ -32,23 +33,31 @@ func simpleSupplyCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 	if sp.Cdata != nil {
 		clen = len(sp.Cdata.Entity)
 	}
- 	log.Printf("SupplyName [%s] arg[%s] len:%d id:%d", sp.SupplyName, sp.ArgJson, clen, sp.SenderId )
+	log.Printf("SupplyName [%s] arg[%s] len:%d id:%d", sp.SupplyName, sp.ArgJson, clen, sp.SenderId)
 }
 
+func simpleDemandCallback(clt *sxutil.SXServiceClient, dm *api.Demand) {
 
+	clen := 0
+	if dm.Cdata != nil {
+		clen = len(dm.Cdata.Entity)
+	}
+	log.Printf("DemandName [%s] arg[%s] len:%d id:%d", dm.DemandName, dm.ArgJson, clen, dm.SenderId)
+}
 
 // SubscribeChannel subscribe selected channel
 func SubscribeChannels(client *sxutil.SXSynerexClient, channelTypes []uint32) {
 
 	for _, ch := range channelTypes {
 		subscFunc, ok := channelSubscribers[int(ch)]
-		chStr := fmt.Sprintf("Clt:ChMon:%d", ch)		
+		chStr := fmt.Sprintf("Clt:ChMon:%d", ch)
 		if ok {
 			chClient := sxutil.NewSXServiceClient(client, uint32(ch), chStr)
 			subscFunc(chClient)
-		}else { // there is no specified viewer
+		} else { // there is no specified viewer
 			chClient := sxutil.NewSXServiceClient(client, uint32(ch), chStr)
-			sxutil.SimpleSubscribeSupply(chClient, simpleSupplyCallback) 
+			sxutil.SimpleSubscribeSupply(chClient, simpleSupplyCallback)
+			sxutil.SimpleSubscribeDemand(chClient, simpleDemandCallback)
 		}
 	}
 
